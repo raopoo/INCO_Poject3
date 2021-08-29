efdb91a6a0d57828c31daa49a253ee24eeae774a
Pooja Rao <raopooj@gmail.com>
2021-08-29 16:43:49 +1000

    display of single user schedule works

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
+const data = require('./data')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-// Get request-route
+// Root 
 app.get('/',(req,res) => {
     res.send('Welcome to our schedule website')
+})
+//Display all users
+app.get('/users',(req,res) =>{
+    res.json(data.users)
+})
+//Display all schedules
+app.get('/schedules',(req,res) =>{
+    res.json(data.schedules)
+})
+
+//Display single user
+app.get('/users/:id',(req,res) => {
+    res.send(data.users[req.params.id]) 
+})
+
+//Display Schedule for a single user
+app.get('/users/:id/schedules',(req,res) => {
+    let schedule = []
+    for(let i = 0;i< data.schedules.length;i++){
+        if(data.schedules[i].user_id === Number(req.params.id)){
+            schedule.push(data.schedules[i])
+        }
+    }
+    // }else{
+    //     res.send('User not found')
+    // }
+    res.send(schedule)
 })
 
 
